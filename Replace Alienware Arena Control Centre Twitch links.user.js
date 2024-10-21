e962e9960cf41d6567749a0a23c3a8a26723b9c0
Adam <dennisaj@outlook.com>
2024-10-21 20:24:24 +0100

    Update for new overlay
    Swaps to component instead of panel.

--- Replace Alienware Arena Control Centre Twitch links.user.js
+++ Replace Alienware Arena Control Centre Twitch links.user.js
@@ -1,16 +1,17 @@
 // ==UserScript==
-// @name        Replace Alienware Arena Control Center Twitch links
-// @description Replaces the Twitch quest links to the Twitch Alienware extension link in the Alienware Arena Control Center Twitch to not require the Twitch stream to be loaded
-// @author      floriegl
+// @name        Replace Alienware Arena Control Centre Twitch Links
+// @description Replaces the Twitch Quest Links with a redirect to the popout so the Twitch Stream is not loaded. 
+// @author      Lone Destroyer
 // @license     CC0
 // @match       https://*.alienwarearena.com/control-center
 // @icon        https://media.alienwarearena.com/images/favicons/favicon.ico
-// @version     1.0
+// @version     2.0
 // @grant       GM_addStyle
 // @run-at      document-start
-// @namespace https://greasyfork.org/users/703184
-// @downloadURL https://update.greasyfork.org/scripts/493363/Replace%20Alienware%20Arena%20Control%20Center%20Twitch%20links.user.js
-// @updateURL https://update.greasyfork.org/scripts/493363/Replace%20Alienware%20Arena%20Control%20Center%20Twitch%20links.meta.js
+// @namespace https://github.com/LoneDestroyer
+// @downloadURL https://raw.githubusercontent.com/LoneDestroyer/Replace-Alienware-Arena-Control-Centre-Twitch-Links/main/Replace%20Alienware%20Arena%20Control%20Centre%20Twitch%20Links.user.js
+// @updateURL https://raw.githubusercontent.com/LoneDestroyer/Replace-Alienware-Arena-Control-Centre-Twitch-Links/main/Replace%20Alienware%20Arena%20Control%20Centre%20Twitch%20Links.user.js
+//Thanks to floriegl for version 1.0
 // ==/UserScript==
 
 (function() {
@@ -37,7 +38,7 @@ const intervalID = setInterval(function () {
         for (const foundLink of foundLinks) {
             const convertedLink = (foundLink.href.match(/(?:https?:\/\/)?(?:www\.)?twitch\.tv\/([^\/?]+)/) || [])[1];
             if (convertedLink != null) {
-                foundLink.href = "https://www.twitch.tv/popout/" + convertedLink + "/extensions/ehc5ey5g9hoehi8ys54lr6eknomqgr/panel";
+                foundLink.href = "https://www.twitch.tv/popout/" + convertedLink + "/extensions/ehc5ey5g9hoehi8ys54lr6eknomqgr/component";
                 foundLink.classList.add("converted");
             }
         }
