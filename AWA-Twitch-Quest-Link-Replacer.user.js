24b234cdb9ab03fc80c559a416dec80af6f7c9b7
Adam <dennisaj@outlook.com>
2025-06-08 17:14:56 +0100

    Removed reliance on selectors
    Twitch renamed a div and caused the detection to mess up for components.
    Now the script fully relies on page Titles.

--- AWA-Twitch-Quest-Link-Replacer.user.js
+++ AWA-Twitch-Quest-Link-Replacer.user.js
@@ -6,7 +6,7 @@
 // @match       https://*.alienwarearena.com/control-center
 // @match       https://*.twitch.tv/popout/*/extensions/ehc5ey5g9hoehi8ys54lr6eknomqgr/component
 // @icon        https://github.com/LoneDestroyer/AWA-Twitch-Quest-Link-Replacer/blob/main/AWALogo.png?raw=true
-// @version     3.1
+// @version     3.2
 // @grant       GM_addStyle
 // @namespace https://github.com/LoneDestroyer
 // @downloadURL https://raw.githubusercontent.com/LoneDestroyer/AWA-Twitch-Quest-Link-Replacer/main/AWA-Twitch-Quest-Link-Replacer.user.js
@@ -48,31 +48,17 @@
         }
         }, 500);
 
-    const currentURL = window.location.href;
-    const title = document.title;
-    // Check if on the component page and the title of the tab is Twitch
-    if (currentURL.endsWith("component") && title == "Twitch") {
-        // Detect if a specific div is present, will retry maxAttempts times then redirect to the panel
-        const targetDivSelector = "#root > div > div.Layout-sc-1xcs6mc-0.gyHpt > div > div > div"; // Checks for the new extension component div
-        let attempts = 0;
-        const maxAttempts = 5; // Retry 5 times (2.5s)
-        const checkInterval = 500; // Check every 500ms (0.5s)
-
-        const checkDivInterval = setInterval(function() {
-            const targetDiv = document.querySelector(targetDivSelector);
-            if (targetDiv) {
-                // If Div is present, do nothing
-                clearInterval(checkDivInterval); // Stop checking
-            } else if (++attempts >= maxAttempts) {
-                // If Div is not present after max attempts, redirect to the panel
-                clearInterval(checkDivInterval);
-                // Extract the channel ID from the URL
-                const channelName = currentURL.split('/popout/')[1].split('/')[0];
-                // Construct the panel URL
-                const panelURL = `https://www.twitch.tv/popout/${channelName}/extensions/ehc5ey5g9hoehi8ys54lr6eknomqgr/panel`;
-                // Redirect to the panel URL
-                window.location.href = panelURL;
-            }
-        }, checkInterval);
-    }
+    setInterval(function() {
+        const currentURL = window.location.href;
+        const title = document.title;
+        // Check if on the component page and the title of the tab is Twitch
+        if (currentURL.endsWith("component") && title !== "Arena Rewards Tracker - Twitch") {
+                    // Extract the channel ID from the URL
+                    const channelName = currentURL.split('/popout/')[1].split('/')[0];
+                    // Construct the panel URL
+                    const panelURL = `https://www.twitch.tv/popout/${channelName}/extensions/ehc5ey5g9hoehi8ys54lr6eknomqgr/panel`;
+                    // Redirect to the panel URL
+                    window.location.href = panelURL;
+        }
+    }, 1000);
 })();
