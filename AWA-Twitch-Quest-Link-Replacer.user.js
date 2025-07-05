c45a0360a40c45ffd45770a1bee7cafd650e7e5b
AyushAfre <75370527+ironman9356@users.noreply.github.com>
2025-07-05 14:12:45 +0530

    Add Automation

--- AWA-Twitch-Quest-Link-Replacer.user.js
+++ AWA-Twitch-Quest-Link-Replacer.user.js
@@ -4,10 +4,11 @@
 // @author      Lone Destroyer
 // @license     MIT
 // @match       https://*.alienwarearena.com/control-center
-// @match       https://*.twitch.tv/popout/*/extensions/ehc5ey5g9hoehi8ys54lr6eknomqgr/component
+// @match       https://*.twitch.tv/popout/*/extensions/ehc5ey5g9hoehi8ys54lr6eknomqgr/*
 // @icon        https://github.com/LoneDestroyer/AWA-Twitch-Quest-Link-Replacer/blob/main/AWALogo.png?raw=true
 // @version     3.3.1
 // @grant       GM_addStyle
+// @grant       GM_xmlhttpRequest
 // @namespace https://github.com/LoneDestroyer
 // @downloadURL https://raw.githubusercontent.com/LoneDestroyer/AWA-Twitch-Quest-Link-Replacer/main/AWA-Twitch-Quest-Link-Replacer.user.js
 // @updateURL https://raw.githubusercontent.com/LoneDestroyer/AWA-Twitch-Quest-Link-Replacer/main/AWA-Twitch-Quest-Link-Replacer.user.js
@@ -64,4 +65,49 @@
             window.location.href = panelURL;
         }
     }, 4000);
+
+    //ADD AUTOMATION
+    // CONSTANTS
+    let onlineStreamers = [];
+    const currentURL = window.location.href;
+    const CHECK_INTERVAL = 10 * 60 * 1000; // 10 mins
+    //TODO: Switch to a x2 ARP Stream by default
+
+    // Get all online streamers
+    function GetOnlineStreamers() {
+        GM_xmlhttpRequest({
+            method: 'GET',
+            url: 'https://www.alienwarearena.com/twitch/live',
+            onload: function(response) {
+                const parser = new DOMParser();
+                const doc = parser.parseFromString(response.responseText, 'text/html');
+                const mediaDivs = doc.querySelectorAll('div.media');
+                mediaDivs.forEach(div => {
+                    const img = div.querySelector('h5');
+                    onlineStreamers.push(img.innerHTML);
+                });
+                console.log(`[AWA-Twitch-Quest-Link-Replacer LOG] List of online streamers`);
+                console.log(onlineStreamers);
+                checkIfOnline();
+            }
+        });
+    }
+
+    // Check if current streamer is online
+    function checkIfOnline() {
+        const channelName = currentURL.split('/popout/')[1].split('/')[0];
+        console.log(`[Automation LOG] Current channel name: ${channelName}`);
+        if (onlineStreamers.includes(channelName)) {
+            console.log('%c%s', 'color:#00FF00',`[AWA-Twitch-Quest-Link-Replacer LOG] Current streamer is online.`);
+        } else {
+            console.log('%c%s', 'color:#ff0000',`[AWA-Twitch-Quest-Link-Replacer LOG] Streamer is offline. Switching to an online streamer...`);
+            const onlineStreamer = onlineStreamers[Math.floor(Math.random() * onlineStreamers.length)];
+            console.log(`[Automation LOG] Switching to ${onlineStreamer}`);
+
+            window.location.href = `https://www.twitch.tv/popout/${onlineStreamer}/extensions/ehc5ey5g9hoehi8ys54lr6eknomqgr/component`;
+        }
+        onlineStreamers.length = 0;
+    }
+
+    setInterval(GetOnlineStreamers, CHECK_INTERVAL);
 })();
